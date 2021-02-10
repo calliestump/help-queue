6f02760b16564a5c61cf801bb96debfde742c5ee
Callie Stump <callie@stu.mp>
2021-02-09 19:06:12 -0500

    adds 'add ticket' functionality
    Co-authored-by: Joseph Nero <josephnero111@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <div class="container">
+        <Header />
+        <TicketControl />
+      </div>
+    </React.Fragment>
+  );
+}
+export default App;
